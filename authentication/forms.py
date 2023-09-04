dfec3a67f35b69448cdb1367f91f422a726f3655
Mona Moxie <moxie4lyf@gmail.com>
2023-09-04 10:01:48 +0200

    auth template

--- authentication/forms.py
+++ authentication/forms.py
@@ -4,6 +4,20 @@ from django.contrib.auth import get_user_model
 
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        label="Email Address",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Email Address"}
+        ),
+    )
+
+    password1 = forms.TextInput(
+        label="Choose a Password",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
+    )
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ("email", "password1", "password2")
--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "assets/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "core", "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
