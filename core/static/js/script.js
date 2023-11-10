71da3f3a17ada6470b4e5821cebb5652506d23f7
Mona Moxie <moxie4lyf@gmail.com>
2023-11-10 19:19:16 +0100

    calendar defaults

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,12 +1,9 @@
 function previewProfilePhoto(input) {
-    console.log('called')
     if (input.files && input.files[0]) {
-        console.log('exists')
         var reader = new FileReader();
         reader.onload = function (e) {
             $('.profile-photo-preview img').attr('src', e.target.result);
         };
-        console.log('read')
         reader.readAsDataURL(input.files[0]);
     }
 }
