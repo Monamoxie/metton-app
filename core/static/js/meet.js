48763eb1688f932a9d7dfefcad6931946a8fe924
Mona Moxie <moxie4lyf@gmail.com>
2023-12-19 18:53:35 +0100

    wip meet calendar

--- core/static/js/meet.js
+++ core/static/js/meet.js
@@ -14,15 +14,39 @@ document.addEventListener('DOMContentLoaded', function() {
             longPressDelay: 200,
             selectLongPressDelay: 200,
             timeZone: getTz(),
-            headerToolbar: { center: 'dayGridMonth', },
+            headerToolbar: { center: 'dayGridMonth,timeGridDay', end: 'prev,next'},
             views: {
                 dayGridMonth: {
+                    titleFormat: { year: 'numeric', month: 'short', day: 'numeric' }
+                },
+                timeGridDay: {
                     titleFormat: { year: 'numeric', month: 'short', day: 'numeric' }
                 }
             },
             validRange: function(nowDate) {
                 return {
                     start: document.getElementById('f_day_month').value,
+                }
+            },
+             events: {
+                url: "/dashboard/events",
+                method: 'GET',
+                extraParams: {
+                    type: 'schedule',
+                },
+                failure: function() {
+                    console.log('there was an error fetching events!');
+                },
+            },
+            datesSet: function (dateInfo) {
+                var view = dateInfo.view;
+
+                if (view.type == "timeGridDay") {
+                    calendar.setOption('selectConstraint', hasBusinessHours ? "businessHours" : null)
+                    calendar.setOption('selectOverlap', false)
+                } else {
+                    calendar.setOption('selectConstraint', null)
+                    calendar.setOption('selectOverlap', true)
                 }
             },
             select: function(info) {
@@ -35,31 +59,40 @@ document.addEventListener('DOMContentLoaded', function() {
                 const [end_date, end_time] = [info.end.toISOString().slice(0, 10), info.end.toISOString().slice(11, 16)]
 
                 if (start_date < today) {
-                    console.log('aaa')
                     return false
                 } 
                 
-                // document.getElementById('eventTitle').innerHTML = info.title
-                // document.getElementById('eventStart').innerHTML = start_date + ' ' + start_time
-                // document.getElementById('eventEnd').innerHTML = end_date + ' ' + end_time
-                // if (info.extendedProps) {
-                //     const timetable = info.extendedProps.timetable
-                //     document.getElementById('eventFreq').innerHTML = 'Every:  ' + timetable
-                // }
-                // // document.getElementById('id_end_time').value = end_time
+                const [start_date_str, end_date_str] = [info.start.toDateString(), info.end.toDateString()]
+                
+                const currentData = calendar.currentData
 
-                // // const e = info.jsEvent
+                if (currentData.currentViewType == "dayGridMonth") {
+                    calendar.changeView('timeGridDay', start_date);
+                } else {
+                    setTimeout(() => {
+                        const myModalEl = document.getElementById('meet-modal')
+                        const myModal = new bootstrap.Modal(myModalEl)
+                        myModal.show()
+            
+                            myModalEl.addEventListener('hidden.bs.modal', event => {
+                                myModal.hide()
+                                calendar.unselect()
+                            })
+                    }, 500);
 
-                // setTimeout(() => {
-                //     const myModalEl = document.getElementById('meet-modal')
-                //     const myModal = new bootstrap.Modal(myModalEl)
-                //     myModal.show()
-          
-                //     myModalEl.addEventListener('hidden.bs.modal', event => {
-                //         myModal.hide()
-                //     })
-                // }, 500);
+                    document.getElementById('evtStartTime').innerHTML = start_time
+                    document.getElementById('evtStartDate').innerHTML = start_date_str
+                    document.getElementById('evtEndTime').innerHTML = end_time
+                    document.getElementById('evtEndDate').innerHTML = end_date_str
+
+                    // document.getElementById('eventEnd').innerHTML = end_date + ' ' + end_time
+                    // if (info.extendedProps) {
+                    //     const timetable = info.extendedProps.timetable
+                    //     document.getElementById('eventFreq').innerHTML = 'Every:  ' + timetable
+                    // }
+                }      
             },
+            slotLabelInterval: "00:30",
             eventColor: '#008000',
             selectOverlap: function(event) {
                 return true
@@ -69,15 +102,38 @@ document.addEventListener('DOMContentLoaded', function() {
             selectMirror: true,
             unselectAuto: false,
             nowIndicator: true,
+            businessHours: hasBusinessHours ? businessHours : false,
         });
         calendar.render(); 
     }
- 
-    renderCalender()
-    setTimeout(() => {
-        const myModalEl = document.getElementById('meet-modal')
-        const myModal = new bootstrap.Modal(myModalEl)
-        myModal.show()
-    }, 500);
+    
+    // pull NBH
+    async function getNbh() {
+        await fetch("/dashboard/business-hours", {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+        }).then((response) => {
+            return response.json()
+        }).then((data) => {
+            if (data.length > 0) {
+                hasBusinessHours = true
+                businessHours = data
+            }
+        }).catch((error) => {
+            console.log(error)
+        }).finally(() => {
+            renderCalender()
+        })
+    }
+
+    getNbh() 
+    // setTimeout(() => {
+    //     const myModalEl = document.getElementById('meet-modal')
+    //     const myModal = new bootstrap.Modal(myModalEl)
+    //     myModal.show()
+    // }, 500);
     
 }); 
