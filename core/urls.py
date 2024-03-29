b3360b483db77f66a3b1aa46e662144fcf7b956b
Mona Moxie <moxie4lyf@gmail.com>
2024-03-29 11:30:54 +0100

    clean up backup task def

--- core/urls.py
+++ core/urls.py
@@ -22,13 +22,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from authentication import views
 
-urlpatterns = (
-    [
-        path("admin/", admin.site.urls),
-        path("auth/", include("authentication.urls")),
-        path("dashboard/", include("dashboard.urls")),
-        path("", include("home.urls")),
-    ]
-    # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls")),
+    path("dashboard/", include("dashboard.urls")),
+    path("", include("home.urls")),
+]
