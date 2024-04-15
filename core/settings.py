b36e0ffbca73013531e7d3de51165c15cc6a0214
Mona Moxie <moxie4lyf@gmail.com>
2024-04-15 08:27:01 +0200

    ignore pyc files  and cache directory

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("APP_SECURITY_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("APP_ENV") != "production"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
