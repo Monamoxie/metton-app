53a3b7acf367c55ad94ac713a397fac0fb470c6f
Mona Moxie <moxie4lyf@gmail.com>
2024-04-22 09:34:49 +0200

    pull default_from_email from env

--- core/settings.py
+++ core/settings.py
@@ -188,3 +188,6 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+# Custom settings
