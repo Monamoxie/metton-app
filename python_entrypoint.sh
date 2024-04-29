df63b9d9b791d5bc52204f954701ab632c71968f
Mona Moxie <moxie4lyf@gmail.com>
2024-04-29 08:29:06 +0200

    add celery worker to entrypoint

--- python_entrypoint.sh
+++ python_entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Read the value of APP_ENV environment variable
 APP_ENV="${APP_ENV:-development}"
-
+celery -A core worker -l info &
 if [ "$APP_ENV" == "production" ]; then
     python manage.py migrate
     gunicorn --bind 0.0.0.0:8000 core.wsgi:application
