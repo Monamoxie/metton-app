44b4bb6a7e4ceb11850e08bd38d2db56d5b587f4
Mona Moxie <moxie4lyf@gmail.com>
2024-05-16 09:50:34 +0200

    force use of ssl in email smtp

--- core/settings.py
+++ core/settings.py
@@ -193,8 +193,7 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
-# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
-EMAIL_USE_SSL = os.environ.get("PROJECT_ENV") == "production"
+EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 # Custom settings
