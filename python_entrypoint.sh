dcad6e465fb5570d4d82616ac825d25eda2de07f
Mona Moxie <moxie4lyf@gmail.com>
2024-06-04 08:40:26 +0200

    debug: issue with static assets not loading on prod

--- python_entrypoint.sh
+++ python_entrypoint.sh
@@ -4,7 +4,6 @@
 PROJECT_ENV="${PROJECT_ENV:-development}"
 celery -A core worker -l info &
 if [ "$PROJECT_ENV" == "production" ]; then
-    python manage.py collectstatic --noinput
     python manage.py migrate
     gunicorn --bind 0.0.0.0:8000 core.wsgi:application
 else
