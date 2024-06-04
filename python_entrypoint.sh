d70923fcf9e92f0b0b99752d56b8c58fd9ac5cb5
Mona Moxie <moxie4lyf@gmail.com>
2024-06-04 20:15:51 +0200

    refactor: reorder flow of execution for entrypoint

--- python_entrypoint.sh
+++ python_entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+python manage.py migrate
+
 # Read the value of PROJECT_ENV environment variable
 PROJECT_ENV="${PROJECT_ENV:-development}"
-celery -A core worker -l info &
 if [ "$PROJECT_ENV" == "production" ]; then
-    python manage.py migrate
+    python manage.py collectstatic --noinput
     gunicorn --bind 0.0.0.0:8000 core.wsgi:application
 else
-    python manage.py migrate
     python manage.py runserver 0.0.0.0:8000
 fi
+
+celery -A core worker -l info &
