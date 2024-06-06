bd5abea1423e844079a6d706ba8f724df635073f
Mona Moxie <moxie4lyf@gmail.com>
2024-06-06 09:23:35 +0200

    #fix: swap and use the correct method in dashboard legacy view

--- dashboard/views_legacy.py
+++ dashboard/views_legacy.py
@@ -12,97 +12,15 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from dashboard.models import User
 from .forms import ScheduleManagerForm
-from .services.eventservice import EventService
+from core.services.event_service import EventService
 from django.contrib.auth import logout as authLogout
 from core.services.email_service import EmailService
 from dashboard.tasks import email_sender
 
 
 @login_required
-def manageSchedule(request):
-    choices = Event().get_frequency_choices
-    if request.method == "POST":
-        form = ScheduleManagerForm(request.POST or None)
-        if form.is_valid():
-            type_input_value = (
-                form.cleaned_data["type"] if "type" in form.cleaned_data else 2
-            )
-
-            user_time_zone = EventService().extract_user_timezone(form.data)
-
-            start_date = form.cleaned_data["start_date"]
-
-            start_time = EventService().timezone_conversion(
-                date_str=str(start_date),
-                time_str=str(form.cleaned_data["start_time"]),
-                from_timezone=user_time_zone,
-                to_timezone="UTC",
-            )
-
-            end_date = form.cleaned_data["end_date"]
-            end_time = EventService().timezone_conversion(
-                date_str=str(end_date),
-                time_str=str(form.cleaned_data["end_time"]),
-                from_timezone=user_time_zone,
-                to_timezone="UTC",
-            )
-
-            frequency = EventService().get_frequency(form.cleaned_data["frequency"])
-
-            if start_time == None or end_time == None:
-                messages.error(
-                    request,
-                    "Start time/End time could not be processed. Please ensure the correct slots were selected",
-                )
-                return redirect("schedule_manager")
-
-            if frequency == "" and (start_date == end_date and start_time > end_time):
-                messages.error(
-                    request,
-                    "Start time cannot be greater than End time, when start date and end date are the same",
-                )
-                return redirect("schedule_manager")
-
-            title = EventService.get_event_title(type_input_value)
-            event = Event(
-                frequency=EventService().get_frequency(form.cleaned_data["frequency"]),
-                user=User.objects.get(id=request.user.id),
-                title=title,
-                type=type_input_value,
-                start_date=start_date,
-                start_time=start_time,
-                end_date=end_date,
-                end_time=end_time,
-                timezone=user_time_zone,
-            )
-            event.save()
-            messages.success(request, "Your schedule has been updated!")
-            return redirect("schedule_manager")
-
-        else:
-            messages.error(request, form.errors)
-    else:
-        form = ScheduleManagerForm(None)
-
-    business_hours = EventService().get_business_hours(user=request.user)
-
-    return render(
-        request,
-        "dashboard/schedule_manager.html",
-        {
-            "form": form,
-            "choices": choices,
-            "weekday_num": datetime.now().isoweekday(),
-            "business_hours": business_hours,
-            "display_name": request.user.name,
-            "position": request.user.position,
-        },
-    )
-
-
-@login_required
 def dashboard(request):
-    choices = Event().get_frequency_choices
+    choices = EventTypes.options()
     if request.method == "POST":
         form = ScheduleManagerForm(request.POST or None)
         if form.is_valid():
