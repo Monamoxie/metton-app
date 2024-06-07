edeaf7f7947308a3d51cfbba10ba7fb35e1ac5b3
Mona Moxie <moxie4lyf@gmail.com>
2024-06-07 09:20:24 +0200

    #refactor: a refactor of the dashboard url routes to use class based views

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,13 +1,18 @@
 from . import views
 from django.urls import path
-from .views import ProfileUpdateView, PasswordUpdateView, ScheduleManagerView
+from .views import (
+    ProfileUpdateView,
+    PasswordUpdateView,
+    ScheduleManagerView,
+    EventListView,
+)
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("profile-update", ProfileUpdateView.as_view(), name="profile_update"),
     path("password-update", PasswordUpdateView.as_view(), name="password_update"),
     path("manage/schedule", ScheduleManagerView.as_view(), name="schedule_manager"),
-    path("events", views.getEvents, name="events"),
+    path("events", EventListView.as_view(), name="events"),
     path("events/detach", views.detachEvent, name="delete-event"),
     path("business-hours", views.getBusinessHours, name="business-hours"),
     path("business-hours/detach", views.detachBusinessHours, name="business-hours"),
