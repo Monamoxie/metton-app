490c09335a9b594033fc2cd4d27a882d76b8c408
Mona Moxie <moxie4lyf@gmail.com>
2024-06-07 09:57:36 +0200

    #refactor: remove the utilites module and switch to core utils

--- core/settings.py
+++ core/settings.py
@@ -97,7 +97,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "core.helpers.utilities.template_data",
+                "core.utils.template_data",
             ],
         },
     },
--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-from core import settings
 from core.data.template_data import TemplateData
 
 
