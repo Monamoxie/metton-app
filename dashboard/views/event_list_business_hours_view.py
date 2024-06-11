d8a7cf3613978d16175f075a4fec738efd66c299
Mona Moxie <moxie4lyf@gmail.com>
2024-06-11 09:08:14 +0200

    feat: setup mypy package

--- dashboard/views/event_list_business_hours_view.py
+++ dashboard/views/event_list_business_hours_view.py
@@ -1,11 +1,12 @@
 from django.views import View
 from core.services import EventService
 from django.http import JsonResponse
+from typing import List, Dict
 
 
 class EventListBusinessHoursView(View):
     def get(self, request):
-        data = EventService.get_business_hours(request.user)
+        data: List[Dict] = EventService.get_business_hours(request.user)
 
         return JsonResponse(
             data=data,
