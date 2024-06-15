254f58f7a33793be35b16bec7949c25b7331cc7d
Mona Moxie <moxie4lyf@gmail.com>
2024-06-15 20:41:15 +0200

    refactor: clear booking view

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,8 @@ from home.views import BookingView
 
 urlpatterns = [
     path("", views_legacy.index, name="index"),
-    path("meet/<public_id>", views_legacy.meet, name="meet"),
-    path("meet/<public_id>/book", BookingView.as_view(), name="book"),
+    path("meet/<str:public_id>", views_legacy.meet, name="meet"),
+    path("meet/<str:public_id>/book", BookingView.as_view(), name="book"),
     path(
         "business-hours/<public_id>",
         views_legacy.getUserBusinessHours,
