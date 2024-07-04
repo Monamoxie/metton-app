b1705ff4a145b969daaeedeafc6b7d16a34d03f7
Mona Moxie <moxie4lyf@gmail.com>
2024-07-04 09:30:26 +0200

    feat: add new field properties to verification token model

--- identity/models/verification_token.py
+++ identity/models/verification_token.py
@@ -7,6 +7,8 @@ from identity.enums import VerificationTokenTypes
 class VerificationToken(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     type = models.CharField(
-        choices=VerificationTokenTypes.options(), null=False, blank=False
+        choices=VerificationTokenTypes.options(), max_length=32, null=False, blank=False
     )
+    token = models.CharField(blank=False, null=False, max_length=256)
+    expires_at = models.DateTimeField(blank=False, null=True, default=None)
     created_at = models.DateTimeField(auto_now_add=True)
