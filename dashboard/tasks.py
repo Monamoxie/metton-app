75af25d398b9a060202a32818c15787f06289b20
Mona Moxie <moxie4lyf@gmail.com>
2024-07-08 08:49:31 +0200

    bugfix: fix method call to email service

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -1,10 +1,10 @@
 from celery.utils.log import get_task_logger
 from core.celery import app
-from core.services.email_service import EmailService
+from core.services import EmailService
 
 
 @app.task()
 def email_sender(subject, recipients, template, context):
-    return EmailService().push(
+    return EmailService.push(
         subject=subject, recipients=recipients, template=template, context=context
     )
