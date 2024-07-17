cb1835bcc1f76afb40b01fcd17417bcae4c81469
Mona Moxie <moxie4lyf@gmail.com>
2024-07-17 09:15:19 +0200

    bugfix: fix login redirect url

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = "dashboard.User"
 
-LOGIN_URL = "/identity/login"
+LOGIN_URL = "/identity/signin"
 
 MESSAGE_TAGS = {
     messages.INFO: "alert alert-primary p-3",
