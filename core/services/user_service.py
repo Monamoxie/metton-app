078d7847868651718fb09709be07b8674ac3678f
Mona Moxie <moxie4lyf@gmail.com>
2024-07-18 09:54:13 +0200

    feat: get user by email

--- core/services/user_service.py
+++ core/services/user_service.py
@@ -1,9 +1,11 @@
 import uuid
-
+from typing import Union
 from dashboard.models.user import User
 
 
 class UserService:
+    NO_USER_FOUND_MESSAGE = "No user found"
+
     def generate_email_verification_link(self): ...
 
     def generate_unique_public_id(self):
@@ -13,3 +15,7 @@ class UserService:
             if not User().objects.filter(public_id=p_id).exists():
                 id_exists = False
         return p_id
+
+    @staticmethod
+    def get_user_by_email(email: str) -> Union[User, None]:
+        return User.objects.filter(email=email).first()
