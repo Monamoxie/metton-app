261c8e7d7bdc4b34caa2b451130fcefda8f27d9b
Mona Moxie <moxie4lyf@gmail.com>
2024-07-19 09:28:29 +0200

    feat: setup the password reset view

--- identity/views/__init__.py
+++ identity/views/__init__.py
@@ -2,4 +2,4 @@ from .signup_view import SignupView
 from .signin_view import SigninView
 from .email_verification_view import EmailVerificationView
 from .forgot_password_view import ForgotPasswordView
-from .password_reset_verification_view import PasswordResetVerificationView
+from .password_reset_view import PasswordResetView
---
+++ identity/views/password_reset_view.py
@@ -0,0 +1,26 @@
+from typing import Any
+from django.views.generic import TemplateView
+
+from identity.enums import VerificationTokenTypes
+from identity.services.verification_token_service import VerificationTokenService
+
+
+class PasswordResetView(TemplateView):
+    """Handles email verification"""
+
+    template_name = "identity/email_verification.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        """Check token and include some extra data, before passing it over to the template"""
+        context = super().get_context_data(**kwargs)
+
+        token = self.kwargs.get("token")
+        status = VerificationTokenService(
+            VerificationTokenTypes.EMAIL_VERIFICATION.value, None
+        ).verify_email_token(token)
+
+        context["valid"] = status == VerificationTokenService.SUCCESS_STATUS
+        context["type"] = "Email"
+        context["token_status"] = status
+
+        return context
