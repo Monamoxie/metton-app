2090e700c285d51421f2d0b8dcaccf1e604a66ac
Mona Moxie <moxie4lyf@gmail.com>
2024-07-25 09:45:04 +0200

    refactor: signup view to use the decoupled function defined in identity app

--- identity/views/signup_view.py
+++ identity/views/signup_view.py
@@ -1,21 +1,13 @@
-from typing import Union
 from django.forms import BaseModelForm
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from dashboard.models import User
-from identity.enums import VerificationTypes
 from identity.forms import SignupForm
 from core.mixins import GuestOnlyMixin
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-import os
-from core import settings
-from dashboard.tasks import email_sender
 from django.shortcuts import redirect
-from django.contrib.auth.models import AbstractUser
-from identity.models import verification_token
-from identity.services.verification_token_service import VerificationTokenService
+from identity.utils import send_signup_email
 
 
 class SignupView(GuestOnlyMixin, CreateView):
@@ -40,7 +32,7 @@ class SignupView(GuestOnlyMixin, CreateView):
             )
             return self.form_invalid(form)
 
-        self._send_signup_email(user)
+        send_signup_email(user=user)
 
         login(self.request, user)
 
@@ -50,21 +42,3 @@ class SignupView(GuestOnlyMixin, CreateView):
     def form_invalid(self, form: BaseModelForm) -> HttpResponse:
         messages.error(self.request, f"{form.errors}")
         return super().form_invalid(form)
-
-    def _send_signup_email(self, user: Union[User, AbstractUser]):
-        """Trigger an email verification event to Celery/RabbitMQ"""
-        service = VerificationTokenService(VerificationTypes.EMAIL_VERIFICATION.value)
-
-        token = service.generate_token(user=user)
-        if token:
-            verification_link = (
-                VerificationTokenService.generate_email_verification_url(token)
-            )
-
-            context = {"verification_link": verification_link}
-
-            template = os.path.join(
-                settings.BASE_DIR,
-                "identity/templates/identity/emails/email_verification.email.html",
-            )
-            email_sender.delay("Email Verification", [user.email], template, context)
