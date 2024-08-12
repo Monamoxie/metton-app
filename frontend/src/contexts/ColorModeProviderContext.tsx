6ffc37c13632f8a791d6238a2590972bbc565f2e
Mona Moxie <moxie4lyf@gmail.com>
2024-08-12 09:56:08 +0200

    wip: set color mode provider context

---
+++ frontend/src/contexts/ColorModeProviderContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import { ColorModeContextProps } from "@/interfaces/theme";
+import { LayoutProps } from "@/interfaces/layout-props";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+export const ColorModeContext = createContext<ColorModeContextProps | null>(null);
+
+export default function ColorModeProviderContext({ children }: LayoutProps) {
+  const [mode, setMode] = useState<"light" | "dark">("dark");
+
+  const toggleColorMode = () => {
+    setMode((prevMode) => (prevMode === "dark" ? "light" : "dark"));
+  };
+
+  const defaultTheme = createTheme({ palette: { mode } });
+
+  const value = {
+    mode,
+    toggleColorMode,
+  };
+
+  return (
+    <ColorModeContext.Provider value={value}>
+      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
