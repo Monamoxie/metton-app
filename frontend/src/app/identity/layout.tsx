64a6593868423b7363c7387a676468587c665292
Mona Moxie <moxie4lyf@gmail.com>
2024-08-12 09:58:05 +0200

    wip: set identity layout template

--- frontend/src/app/identity/layout.tsx
+++ frontend/src/app/identity/layout.tsx
@@ -1,7 +1,43 @@
-import Image from "next/image";
+"use client";
+
 import Button from "@mui/material/Button";
-import { LayoutProps } from "@/interfaces/layout_props";
+import { LayoutProps } from "@/interfaces/layout-props";
+import Stack from "@mui/material/Stack";
+import ArrowBackRoundedIcon from "@mui/icons-material/ArrowBackRounded";
+import ToggleColorMode from "@/app/identity/signup/ToggleColorMode";
+import * as React from "react";
+import useColorMode  from "@/hooks/useColorMode";
 
 export default function IdentityLayout(props: LayoutProps) {
-  return <div className="identity">{props.children}</div>;
+  const { mode, toggleColorMode } = useColorMode();
+
+  return (
+    <div className="identity">
+      <Stack
+        direction="row"
+        sx={{
+          justifyContent: "space-between",
+          position: { sm: "static", md: "fixed" },
+          width: "100%",
+          p: { xs: 2, sm: 4 },
+        }}
+      >
+        {
+          <Button
+            startIcon={<ArrowBackRoundedIcon />}
+            component="a"
+            href="/material-ui/getting-started/templates/"
+          >
+            Home
+          </Button>
+        }
+        <ToggleColorMode
+          data-screenshot="toggle-mode"
+          mode={mode}
+          toggleColorMode={toggleColorMode}
+        />
+      </Stack>
+      {props.children}
+    </div>
+  );
 }
