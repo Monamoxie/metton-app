5f4d780432d91599656dc648838e8ad874d18ac2
Mona Moxie <moxie4lyf@gmail.com>
2024-08-30 21:06:17 +0200

    feat: create signup api view

--- identity/views/api/signup_view.py
+++ identity/views/api/signup_view.py
@@ -1,13 +1,19 @@
+from datetime import datetime, timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate, login
-from rest_framework_simplejwt.tokens import RefreshToken
+from core.message_bag import MessageBag
 from dashboard.models.user import User
-from identity.serializers import SignupSerializer
+from identity.serializers import SignupSerializer, UserSerializer
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.tokens import Token
+from knox.views import LoginView as KnoxLoginView
+from knox.models import AuthToken
+from rest_framework import generics
+from knox.settings import CONSTANTS, knox_settings
+from django.utils import timezone
+from rest_framework.authtoken.models import Token
+from identity.utils import send_signup_email
 
 User = get_user_model()
 
@@ -20,16 +26,16 @@ class SignupView(APIView):
         if serializer.is_valid():
             user = serializer.save()
 
+            user_data = UserSerializer(user).data
+
             if isinstance(user, User):
-                refresh = RefreshToken.for_user(user)
+                send_signup_email(user=user)
                 return Response(
                     {
-                        "user": {
-                            "email": user.email,
-                        },
-                        "refresh": str(refresh),
-                        "access": str(refresh.access_token),
+                        "user": user_data,
+                        "_message": MessageBag.CREATED_SUCCESSFULLY.format(data="User"),
                     },
                     status=status.HTTP_201_CREATED,
                 )
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
