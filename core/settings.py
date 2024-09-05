4beacdfcf10eaedf7f493abf593f99dd3158994c
Mona Moxie <moxie4lyf@gmail.com>
2024-09-05 09:40:44 +0200

    feat: update knox token ttl

--- core/settings.py
+++ core/settings.py
@@ -216,11 +216,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-SIMPLE_JWT = {
-    # todo :::
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
-    "BLACKLIST_AFTER_ROTATION": True,
-    "AUTH_HEADER_TYPES": ("Bearer",),
+REST_KNOX = {
+    "TOKEN_TTL": timedelta(hours=1),
 }
