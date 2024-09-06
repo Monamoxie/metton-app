5991d4d995684dae7030a694f79fc8fb72ff81aa
Mona Moxie <moxie4lyf@gmail.com>
2024-09-06 20:16:52 +0200

    feat: update user serializer

--- identity/serializers/__init__.py
+++ identity/serializers/__init__.py
@@ -1,2 +1,4 @@
 from .signup_serializer import SignupSerializer
+from .signin_serializer import SignInSerializer
 from .user_serializer import UserSerializer
+from .auth_token_serializer import AuthTokenSerializer
--- identity/serializers/user_serializer.py
+++ identity/serializers/user_serializer.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-
+from knox.models import AuthToken
 from dashboard.models.user import User
+from .auth_token_serializer import AuthTokenSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField(required=False, allow_null=True)
+    tokens = serializers.SerializerMethodField(required=False)
+
     class Meta:
         model = User
         fields = [
@@ -14,4 +17,22 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "is_active",
             "date_joined",
+            "token",
+            "tokens",
         ]
+
+    def get_token(self, obj):
+        """Return the token if provided in the context."""
+       
+        token = self.context.get("token")  # Retrieve token from context if set
+        return AuthTokenSerializer(token).data if token else None
+
+    def get_tokens(self, obj):
+        tokens = AuthToken.objects.filter(user=obj)
+        return AuthTokenSerializer(tokens, many=True).data
+
+    def __init__(self, *args, **kwargs):
+        token = kwargs.pop("token", None)
+        super(UserSerializer, self).__init__(*args, **kwargs)
+        if token:
+            self.context["token"] = AuthTokenSerializer(token)
