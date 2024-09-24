ee8fe09b9984327116d5e86ee99d517103d1afe8
Mona Moxie <moxie4lyf@gmail.com>
2024-09-24 09:16:57 +0200

    feat: include message on successful sign up

--- frontend/src/components/identity/SignUpCard.tsx
+++ frontend/src/components/identity/SignUpCard.tsx
@@ -3,25 +3,27 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { Card, CircularProgress, Stack } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
+import {
+  Alert,
+  AlertTitle,
+  Card,
+  CircularProgress,
+  Stack,
+} from "@mui/material";
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Link from "@mui/material/Link";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { Identity_Form_Card_Css } from "@/styles/modules/identity/identity-layout.css";
-import ForgotPassword from "../../app/identity/signup/ForgotPassword";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { signupSchema } from "@/schemas/identity";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useTranslations } from "next-intl";
-import { signupAction } from "@/data/identity/signup-action";
 import { useState } from "react";
 export type SignupInputs = z.infer<ReturnType<typeof signupSchema>>;
 import ErrorDisplay from "@/components/ErrorDisplay";
+import Confetti from "../magicui/confetti";
 
 export default function SignUpCard() {
   const t = useTranslations();
@@ -29,9 +31,7 @@ export default function SignUpCard() {
   const [responseErrors, setResponseErrors] = useState<{
     [key: string]: string[];
   }>({});
-  const [responseData, setResponseData] = useState<{
-    [key: string]: string[];
-  }>({});
+  const [isFinished, setIsFinished] = useState(false);
   const [processing, setProcessing] = useState(false);
 
   const {
@@ -43,7 +43,6 @@ export default function SignUpCard() {
   });
 
   const onSubmit: SubmitHandler<SignupInputs> = async (data) => {
-    // const response = await signupAction(data);
     setProcessing(true);
     const request = await fetch("/api/identity/signup", {
       method: "POST",
@@ -60,51 +59,46 @@ export default function SignUpCard() {
     const response = await request.json();
     setProcessing(false);
 
-    if (!request.ok) {
-      console.error(response.errors);
-    } else {
-      console.log("Login successful!!!", response);
-    }
-
-    response.code !== 200
-      ? setResponseErrors(response.errors)
-      : setResponseData(response.data);
+    !request.ok ? setResponseErrors(response.errors) : setIsFinished(true);
   };
 
   return (
     <Stack direction="column" sx={Identity_Form_Card_Css}>
-      <Card className="identity-form-card">
-        <Typography component="h1" variant="h4" className="card-title">
-          Create Account
-        </Typography>
-        {responseErrors && <ErrorDisplay errors={responseErrors} />}
-        <Box
-          component="form"
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-          className="card-content"
-        >
-          <FormControl>
-            <FormLabel htmlFor="email">Email</FormLabel>
-            <TextField
-              {...register("email")}
-              error={!!errors.email}
-              helperText={(errors.email?.message as string) || ""}
-              id="email"
-              type="email"
-              placeholder={t("prompts.ENTER_YOUR_DATA", {
-                data: "email address",
-              })}
-              autoComplete="true"
-              autoFocus
-              required
-              sx={{ ariaLabel: "email" }}
-            />
-          </FormControl>
-          <FormControl>
-            <Box className="password-label-wrap">
-              <FormLabel htmlFor="password">Password</FormLabel>
-              {/* <Link
+      {isFinished && getCompletedContent()}
+
+      {!isFinished && (
+        <Card className="identity-form-card">
+          <Typography component="h1" variant="h4" className="card-title">
+            Create Account
+          </Typography>
+          {responseErrors && <ErrorDisplay errors={responseErrors} />}
+          <Box
+            component="form"
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            className="card-content"
+          >
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                {...register("email")}
+                error={!!errors.email}
+                helperText={(errors.email?.message as string) || ""}
+                id="email"
+                type="email"
+                placeholder={t("prompts.ENTER_YOUR_DATA", {
+                  data: "email address",
+                })}
+                autoComplete="true"
+                autoFocus
+                required
+                sx={{ ariaLabel: "email" }}
+              />
+            </FormControl>
+            <FormControl>
+              <Box className="password-label-wrap">
+                <FormLabel htmlFor="password">Password</FormLabel>
+                {/* <Link
                 component="button"
                 // onClick={handleClickOpen}
                 variant="body2"
@@ -112,48 +106,49 @@ export default function SignUpCard() {
               >
                 Forgot your password?
               </Link> */}
-            </Box>
-            <TextField
-              {...register("password")}
-              error={!!errors.password}
-              helperText={(errors.password?.message as string) || ""}
-              placeholder="••••••"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              autoFocus
-              required
-              sx={{ ariaLabel: "password" }}
-            />
-          </FormControl>
+              </Box>
+              <TextField
+                {...register("password")}
+                error={!!errors.password}
+                helperText={(errors.password?.message as string) || ""}
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                autoFocus
+                required
+                sx={{ ariaLabel: "password" }}
+              />
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="password-conf">Re-enter Password</FormLabel>
-            <TextField
-              {...register("password_conf")}
-              error={!!errors.password_conf}
-              helperText={(errors.password_conf?.message as string) || ""}
-              name="password_conf"
-              placeholder="••••••"
-              type="password"
-              id="password-conf"
-              autoComplete="confirm-password"
-              autoFocus
-              required
-              sx={{ ariaLabel: "password-conf" }}
-            />
-          </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password-conf">Re-enter Password</FormLabel>
+              <TextField
+                {...register("password_conf")}
+                error={!!errors.password_conf}
+                helperText={(errors.password_conf?.message as string) || ""}
+                name="password_conf"
+                placeholder="••••••"
+                type="password"
+                id="password-conf"
+                autoComplete="confirm-password"
+                autoFocus
+                required
+                sx={{ ariaLabel: "password-conf" }}
+              />
+            </FormControl>
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            disabled={processing}
-          >
-            {getButtonContent(processing)}
-          </Button>
-        </Box>
-      </Card>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              disabled={processing}
+            >
+              {getButtonContent(processing)}
+            </Button>
+          </Box>
+        </Card>
+      )}
     </Stack>
   );
 }
@@ -163,6 +158,26 @@ function getButtonContent(processing: boolean): JSX.Element | string {
     return <CircularProgress size={22} sx={{ color: "#fff" }} />;
   }
   return "Create Account";
+}
+
+function getCompletedContent(): JSX.Element {
+  return (
+    <>
+      <Confetti />
+      <Alert sx={{ p: 5 }} severity="success">
+        <AlertTitle>
+          <h1>Welcome to Metton!</h1>
+        </AlertTitle>
+        <Typography sx={{ pt: 2 }} variant="subtitle1" component="p">
+          Your account has been successfully created.
+        </Typography>
+        <Typography sx={{ pt: 2 }} variant="subtitle1" component="p">
+          A confirmation link has just been sent to your email address. <br />
+          Click on the confirmation link to get started.
+        </Typography>
+      </Alert>
+    </>
+  );
 }
 
 // {/* <ForgotPassword open={open} handleClose={handleClose} /> */}
