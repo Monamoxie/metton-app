e05e59d1f00193e76b737e0b564f983ce0e87144
Mona Moxie <moxie4lyf@gmail.com>
2024-09-25 09:57:18 +0200

    refactor: alter api url for email verification

--- identity/api_urls.py
+++ identity/api_urls.py
@@ -12,7 +12,7 @@ from identity.views.api import (
 # API Routes ::: Migration to DRF in progress
 urlpatterns = [
     path("signup", SignupView.as_view()),
-    path("email/verify", EmailVerificationView.as_view()),
+    path("verification/email", EmailVerificationView.as_view()),
     path("signin", SignInView.as_view()),
     path("forgot-password", ForgotPasswordView.as_view()),
     path("password-reset/verify", PasswordResetVerificationView.as_view()),
