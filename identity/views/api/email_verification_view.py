0d2fa2228d590fb9b243764ae354ead82ee4bd20
Mona Moxie <moxie4lyf@gmail.com>
2024-10-09 09:21:46 +0200

    refactor: set token validation message to a more verbose message

--- identity/views/api/email_verification_view.py
+++ identity/views/api/email_verification_view.py
@@ -1,4 +1,5 @@
 from typing import Union
+from urllib import response
 from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -22,16 +23,20 @@ class EmailVerificationView(UpdateAPIView):
             verification_type = VerificationTypes.EMAIL_VERIFICATION.value
             service = VerificationTokenService(verification_type)
 
-            response = service.verify_email_token(request.data.get("token"))
-            return Response(
-                {
-                    "token": response,
-                },
-                status=(
-                    status.HTTP_200_OK
-                    if response == VerificationTokenService.SUCCESS_STATUS
-                    else status.HTTP_400_BAD_REQUEST
-                ),
+            verification_message = service.verify_email_token(request.data.get("token"))
+
+            response = {}
+            status_code = (
+                status.HTTP_200_OK
+                if response == VerificationTokenService.SUCCESS_STATUS
+                else status.HTTP_400_BAD_REQUEST
             )
+
+            if verification_message == VerificationTokenService.SUCCESS_STATUS:
+                response["_message"] = verification_message
+            else:
+                response["token_error"] = verification_message
+
+            return Response(response, status=status_code)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
