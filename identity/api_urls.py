e32c3d10d83a342d75dbea96c820e1bade66d813
Mona Moxie <moxie4lyf@gmail.com>
2024-10-21 08:30:08 +0200

    modify: password reset api url

--- identity/api_urls.py
+++ identity/api_urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path("verification/email", EmailVerificationView.as_view()),
     path("signin", SignInView.as_view()),
     path("forgot-password", ForgotPasswordView.as_view()),
-    path("password-reset/verify", PasswordResetVerificationView.as_view()),
+    path("verification/password-reset", PasswordResetVerificationView.as_view()),
     path("password-reset", PasswordResetView.as_view()),
 ]
