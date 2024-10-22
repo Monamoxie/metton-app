95b57fb9b21062484934ec6e501476766e4e7534
Mona Moxie <moxie4lyf@gmail.com>
2024-10-22 09:57:07 +0200

    refactor: password reset as main, instead of using as sub route of verification

---
+++ frontend/src/app/api/identity/password-reset/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import { passwordReset } from "@/app/api/identity/identity-fetcher";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { token, password, password_confirmation } = body;
+
+  const response = await passwordReset({
+    token,
+    password,
+    password_confirmation,
+  });
+
+  console.log("aeeeeee", response);
+
+  return NextResponse.json(response);
+}
