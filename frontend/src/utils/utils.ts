4430233bc5de5ab16bf954a1139c937593eb8fcb
Mona Moxie <moxie4lyf@gmail.com>
2024-10-22 09:58:11 +0200

    feat: introduce uitlity local api-request  handler

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,6 +1,7 @@
 import { ApiResponse } from "@/types/api";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { Dispatch, SetStateAction } from "react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -21,4 +22,61 @@ export function getDefaultApiHeader(): HeadersInit {
     "Content-Type": "application/json",
     "Cache-Control": "no-store, max-age=0",
   };
+}
+
+type Method = "POST" | "GET" | "PATCH" | "PUT" | "DELETE";
+interface localApiRequestProps {
+  url: string;
+  method: Method;
+  body: object | null;
+  setProcessing: Dispatch<SetStateAction<boolean>>;
+  setResponseErrors: Dispatch<SetStateAction<{ [key: string]: string[] }>>;
+  setIsFinished: Dispatch<SetStateAction<boolean>>;
+  setMessage?: Dispatch<SetStateAction<string>>;
+}
+
+export async function localApiRequest<T>({
+  url,
+  method,
+  body,
+  setProcessing,
+  setResponseErrors,
+  setIsFinished,
+  setMessage,
+}: localApiRequestProps): Promise<T | null> {
+  setProcessing(true);
+  console.log(body);
+
+  try {
+    const response = await fetch(url, {
+      method,
+      headers: getDefaultApiHeader(),
+      body: body ? JSON.stringify(body) : null,
+    });
+
+    const data = await response.json();
+    setProcessing(false);
+
+    if (response.ok) {
+      setIsFinished(true);
+      if (setMessage && data.message) {
+        setMessage(data.message);
+      }
+      return data;
+    }
+
+    setResponseErrors(
+      data.errors || {
+        generic: [
+          "Something went wrong. Could not contact the outbound server",
+        ],
+      }
+    );
+    return null;
+  } catch (error) {
+    setProcessing(false);
+    console.log(error);
+    setResponseErrors({ generic: ["Network error"] });
+    return null;
+  }
 }
