b27342d0dfe9d723d5b1ec99b7bc27cfab202b6d
Mona Moxie <moxie4lyf@gmail.com>
2024-11-21 09:52:57 +0100

    feat: add global type for mui box

--- frontend/src/types/material-ui.d.ts
+++ frontend/src/types/material-ui.d.ts
@@ -1,7 +1,7 @@
 import "@mui/material/Stack";
 import "@mui/material/Container";
 import { SxProps, Theme } from "@mui/system";
-
+import "@mui/material/Box";
 declare module "@mui/material/Stack" {
   interface StackOwnProps {
     sx?: SxProps<Theme> | ((theme: Theme) => SxProps<Theme>);
@@ -10,6 +10,12 @@ declare module "@mui/material/Stack" {
 
 declare module "@mui/material/Container" {
   interface ContainerOwnProps {
+    sx?: SxProps<Theme> | ((theme: Theme) => SxProps<Theme>);
+  }
+}
+
+declare module "@mui/material/Box" {
+  interface BoxProps {
     sx?: SxProps<Theme> | ((theme: Theme) => SxProps<Theme>);
   }
 }
