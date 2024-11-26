62308fb70b03d335a0f0b39c2c05841fe1acc55c
Mona Moxie <moxie4lyf@gmail.com>
2024-11-26 09:30:14 +0100

    feat: user profile page

---
+++ frontend/src/app/dashboard/user/profile/page.tsx
@@ -0,0 +1,32 @@
+import "server-only";
+
+import ProfileCard from "@/components/dashboard/user/ProfileCard";
+import { getUserProfile } from "@/data/outbound/identity-fetcher";
+import { Container } from "@mui/material";
+import ErrorDisplay from "@/components/ErrorDisplay";
+import { UserProfile } from "@/types/identity";
+
+export default async function ProfilePage() {
+  const response = await getUserProfile();
+  let errorContent = null;
+
+  if (response.code !== 200) {
+    errorContent = <ErrorDisplay errors={response.errors} />;
+  } else {
+    let data = response.data;
+    if (data.profile_photo && data.profile_photo.trim() != "") {
+      data["profile_photo"] = process.env.TEMP_APP_URL + data.profile_photo;
+    }
+  }
+
+  return (
+    <Container>
+      {errorContent || (
+        <ProfileCard
+          user={response.data}
+          base_url={process.env.APP_URL ?? null}
+        />
+      )}
+    </Container>
+  );
+}
