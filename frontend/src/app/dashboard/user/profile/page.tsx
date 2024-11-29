532b31d04e1b6ab7ad869e9e19cbd69bd909bbca
Mona Moxie <moxie4lyf@gmail.com>
2024-11-29 09:32:21 +0100

    refactor: adjust profile page

--- frontend/src/app/dashboard/user/profile/page.tsx
+++ frontend/src/app/dashboard/user/profile/page.tsx
@@ -1,10 +1,7 @@
-import "server-only";
-
 import ProfileCard from "@/components/dashboard/user/ProfileCard";
 import { getUserProfile } from "@/data/outbound/identity-fetcher";
 import { Container } from "@mui/material";
 import ErrorDisplay from "@/components/ErrorDisplay";
-import { UserProfile } from "@/types/identity";
 
 export default async function ProfilePage() {
   const response = await getUserProfile();
@@ -14,19 +11,20 @@ export default async function ProfilePage() {
     errorContent = <ErrorDisplay errors={response.errors} />;
   } else {
     let data = response.data;
-    if (data.profile_photo && data.profile_photo.trim() != "") {
+
+    if (data.profile_photo?.trim()) {
       data["profile_photo"] = process.env.TEMP_APP_URL + data.profile_photo;
     }
   }
 
   return (
     <Container>
-      {errorContent || (
-        <ProfileCard
-          user={response.data}
-          base_url={process.env.APP_URL ?? null}
-        />
-      )}
+        {errorContent || (
+          <ProfileCard
+            user={response.data}
+            base_url={process.env.APP_URL ?? null}
+          />
+        )}
     </Container>
   );
 }
