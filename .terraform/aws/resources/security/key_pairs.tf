778d56b045580b956d3e3973191f19223c646034
Mona Moxie <moxie4lyf@gmail.com>
2025-01-16 09:43:37 +0100

    infras: set key pairs

---
+++ .terraform/aws/resources/security/key_pairs.tf
@@ -0,0 +1,13 @@
+# Default keypair to access your ec2 instance
+# This should be created on your dev machine/workstation, prior to running terraform
+# Create key pair and follow the prompt: ssh-keygen -t ed25519 -C "name-or-identifier"
+# Add path to file to ~/.aws/credentials, under the profile specified in main.tf
+
+resource "aws_key_pair" "metton_ec2_key_pair" {
+  key_name   = "metton-ec2-key-pair"
+  public_key = file(var.key_pair_filename)
+}
+
+output "key_name" {
+  value = aws_key_pair.metton_ec2_key_pair.key_name
+}
