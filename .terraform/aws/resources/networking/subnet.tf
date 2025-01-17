8bc052d8cae8d2fec553da23b22a2e8304348c9e
Mona Moxie <moxie4lyf@gmail.com>
2025-01-17 09:42:20 +0100

    infras: aws subnet

---
+++ .terraform/aws/resources/networking/subnet.tf
@@ -0,0 +1,31 @@
+/**
+ * ****************************************************************************
+ * SUB NET
+ * ****************************************************************************
+*/
+resource "aws_subnet" "default" {
+  vpc_id            = var.vpc_id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "default"
+  }
+}
+
+resource "aws_subnet" "default_2" {
+  vpc_id            = var.vpc_id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "second-subnet"
+  }
+}
+
+resource "aws_subnet" "default_3" {
+  vpc_id            = var.vpc_id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+  tags = {
+    Name = "third-subnet"
+  }
+}
---
+++ .terraform/aws/resources/networking/variables.tf
@@ -0,0 +1,3 @@
+variable "vpc_id" {
+  description = "The ID of the VPC where the security group will be created"
+}
