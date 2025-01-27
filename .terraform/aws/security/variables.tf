d4a055b5d140cd4430a995156c2925efd119b2f8
Mona Moxie <moxie4lyf@gmail.com>
2025-01-27 20:58:47 +0100

    infras: update variables tf

--- .terraform/aws/security/variables.tf
+++ .terraform/aws/security/variables.tf
@@ -6,3 +6,13 @@ variable "key_name" {
   description = "The name of the SSH key pair"
   type        = string
 }
+
+variable "domain_name" {
+  description = "The domain name for Route53 records"
+  type        = string
+}
+
+variable "domain_alternative_names" {
+  description = "The alternative domain names"
+  type        = list(string)
+}
