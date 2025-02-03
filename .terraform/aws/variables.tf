da61c4cb7725a6b36af0bc8e5150f0f88d6e6cb0
Mona Moxie <moxie4lyf@gmail.com>
2025-02-03 09:34:40 +0100

    infras: include main tf variables

--- .terraform/aws/variables.tf
+++ .terraform/aws/variables.tf
@@ -3,3 +3,38 @@ variable "environment" {
   type        = string
   default     = "development"
 }
+
+variable "domain_name" {
+  description = "Production domain name"
+  type = string
+}
+
+variable "domain_alternative_names" {
+  description = "Production subdomains or wildcard"
+  type = string
+}
+
+variable "profile" {
+  description = "Aws profile. Set in /Users/admin/.aws/config or whereever your development aws config is stored. To also be referenced and defined in terraform.tfvars, which MUST NOT be tracked by git"
+  type = string
+}
+
+variable "aws_region" {
+  description = "Aws region"
+  type = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type = string
+}
+
+variable "shared_config_files" {
+  description = "Path to aws config. Could be /Users/admin/.aws/config or wherever you've created it. Should contain the aws profile and the region"
+  type = string
+}
+
+variable "shared_credentials_files" {
+  description = "Credentials file. Coule be /Users/admin/.aws/credentials or wherever you've created it. Should be grouped by aws profile, and this profile should contain the access key and secret required for terraform to authenticate and access the account"
+  type = string
+}
