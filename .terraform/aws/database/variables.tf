a954e98dff1d271f2b4b264ea11710f61996158f
Mona Moxie <moxie4lyf@gmail.com>
2025-02-03 09:12:46 +0100

    infras: fix issue with duplicate variable names

--- .terraform/aws/database/variables.tf
+++ .terraform/aws/database/variables.tf
@@ -20,11 +20,6 @@ variable "security_group_id" {
   type        = string
 }
 
-variable "security_group_id" {
-  description = "ID of the security group for the database"
-  type        = string
-}
-
 variable "db_subnet_group" {
   description = "DB subnet group name"
   type        = string
