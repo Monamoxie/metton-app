e68e29812a2938c56fc1ed9a6b7d5a1da8ade5dc
Mona Moxie <moxie4lyf@gmail.com>
2025-02-05 08:43:36 +0100

    terraform: fix security group rules

--- .terraform/aws/security/security_groups.tf
+++ .terraform/aws/security/security_groups.tf
@@ -43,24 +43,12 @@ resource "aws_vpc_security_group_egress_rule" "outbound_ipv6" {
  * ****************************************************************************
 */
 resource "aws_security_group_rule" "inbound_http_ipv4" {
+    type              = "ingress"
+    protocol          = "tcp"
     security_group_id = aws_security_group.default.id
-    cidr_ipv4         = "0.0.0.0/0"
+    cidr_blocks       = ["0.0.0.0/0"]
     from_port         = 80
     to_port           = 80
-    ip_protocol       = "tcp"
-}
-
-/**
- * ****************************************************************************
- * INCOMING CONNECTION ::: PORT 80 IPV6
- * ****************************************************************************
-*/
-resource "aws_security_group_rule" "inbound_http_ipv6" {
-  security_group_id = aws_security_group.default.id
-  cidr_ipv6         = "::/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
 }
 
 
@@ -71,24 +59,13 @@ resource "aws_security_group_rule" "inbound_http_ipv6" {
 */
 resource "aws_security_group_rule" "inbound_https_ipv4" {
     security_group_id = aws_security_group.default.id
-    cidr_ipv4         = "0.0.0.0/0"
+    type              = "ingress"
+    cidr_blocks       = ["0.0.0.0/0"]
     from_port         = 443
     to_port           = 443
-    ip_protocol       = "tcp"
+    protocol       = "tcp"
 }
 
-/**
- * ****************************************************************************
- * INCOMING CONNECTION ::: PORT 443 IPV6
- * ****************************************************************************
-*/
-resource "aws_security_group_rule" "inbound_https_ipv6" {
-    security_group_id = aws_security_group.default.id
-    cidr_ipv6         = "::/0"
-    from_port         = 443
-    to_port           = 443
-    ip_protocol       = "tcp"
-}
 
 /**
  * ****************************************************************************
@@ -97,11 +74,12 @@ resource "aws_security_group_rule" "inbound_https_ipv6" {
 */
 
 resource "aws_security_group_rule" "inbound_mysql" {
+    type              = "ingress"
     security_group_id = aws_security_group.default.id
-    cidr_ipv4         = "0.0.0.0/0"
-    from_port         = 3306
-    to_port           = 3306
-    ip_protocol       = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+    from_port         = 5432
+    to_port           = 5432
+    protocol          = "tcp"
 }
 
 /**
@@ -110,11 +88,12 @@ resource "aws_security_group_rule" "inbound_mysql" {
  * ****************************************************************************
 */
 resource "aws_security_group_rule" "inbound_ssh" {
+    type              = "ingress"
     security_group_id = aws_security_group.default.id
     from_port         = 22
     to_port           = 22
-    ip_protocol       = "tcp"
-    cidr_ipv4         = "0.0.0.0/0"
+    protocol       = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
@@ -125,11 +104,12 @@ resource "aws_security_group_rule" "inbound_ssh" {
  * ****************************************************************************
 */
 resource "aws_security_group_rule" "inbound_icmp" {
+    type              = "ingress"
     security_group_id = aws_security_group.default.id
     from_port         = -1
     to_port           = -1
-    ip_protocol          = "icmp"
-    cidr_ipv4         = "0.0.0.0/0"
+    protocol       = "icmp"
+    cidr_blocks       = ["0.0.0.0/0"]
     description       = "Allow ICMP (ping) from anywhere"
 }
 
