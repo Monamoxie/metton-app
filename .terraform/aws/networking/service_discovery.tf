3ee8ee25d1e2765972f1ff1c8e6dae54e1fd8d15
Mona Moxie <moxie4lyf@gmail.com>
2025-02-06 09:52:05 +0100

    terraform: aws service discovery

---
+++ .terraform/aws/networking/service_discovery.tf
@@ -0,0 +1,21 @@
+resource "aws_service_discovery_service" "default" {
+  name = "ecs-discovery-service"
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.example.id
+    
+    dns_records {
+      type = "A"
+      ttl  = 10
+    }
+  }
+}
+ 
+resource "aws_service_discovery_private_dns_namespace" "default" {
+  name = "default.local"
+  vpc  = var.vpc_id   
+}
+
+output "service_discovery_arn" {
+  description = "Service discovery arn"
+  value = aws_service_discovery_service.default.arn
+}
