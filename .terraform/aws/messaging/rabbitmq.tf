f1df710810502a516f70a7768309d6614388177e
Mona Moxie <moxie4lyf@gmail.com>
2025-02-07 09:57:29 +0100

    terraform: update rabbit mq engine

--- .terraform/aws/messaging/rabbitmq.tf
+++ .terraform/aws/messaging/rabbitmq.tf
@@ -1,7 +1,7 @@
 resource "aws_mq_broker" "default" {
   broker_name = "default"
   engine_type = "RabbitMQ"
-  engine_version = "3.8.6"  
+  engine_version = "3.12.13"  
   host_instance_type = "mq.t2.micro"
   publicly_accessible = true
 
