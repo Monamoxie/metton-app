9be1fffb13e098f19ce5941f92e58c084bac0a1d
Mona Moxie <moxie4lyf@gmail.com>
2025-02-10 09:05:18 +0100

    terraform: update rabbit mq instance type

--- .terraform/aws/messaging/rabbitmq.tf
+++ .terraform/aws/messaging/rabbitmq.tf
@@ -2,7 +2,7 @@ resource "aws_mq_broker" "default" {
   broker_name = "default"
   engine_type = "RabbitMQ"
   engine_version = "3.12.13"  
-  host_instance_type = "mq.t2.micro"
+  host_instance_type = "mq.t3.micro"
   publicly_accessible = true
 
   user {
