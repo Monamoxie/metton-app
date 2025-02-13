75d056217a279fb7ac6a9b99587b707cc46c7a31
Mona Moxie <moxie4lyf@gmail.com>
2025-02-13 09:55:15 +0100

    terraform: update db

--- .terraform/aws/database/postgres.tf
+++ .terraform/aws/database/postgres.tf
@@ -3,13 +3,13 @@ resource "aws_db_instance" "default" {
     allocated_storage    = 20
     db_name              = var.db_name
     engine               = "postgres"
-    engine_version       = "17.2-R1"
+    engine_version       = "17.2"
     instance_class       = "db.t3.micro"
  
     vpc_security_group_ids = [var.security_group_id]
     db_subnet_group_name   = var.db_subnet_group
   
-    username             = var.db_username
+    username             = var.db_user
     password             = var.db_password
 
   parameter_group_name = "default.postgres15"
