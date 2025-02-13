81795bba9699b8a4b9248896d692318151e3c8e1
Mona Moxie <moxie4lyf@gmail.com>
2025-02-13 09:56:08 +0100

    terraform: add ecr lifecycle policy

--- .terraform/aws/compute/ecr.tf
+++ .terraform/aws/compute/ecr.tf
@@ -5,3 +5,38 @@ resource "aws_ecr_repository" "default" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "default" {
+  repository = aws_ecr_repository.default.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Delete untagged images older than 24 hours"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep only the latest tagged image for each service"
+        selection = {
+          tagStatus   = "tagged"
+          countType   = "imageCountMoreThan"
+          tagPrefixList = ["latest"]
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
