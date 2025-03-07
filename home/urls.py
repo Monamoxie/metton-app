61154c9aae64992bb1af3fcf95be48a3fd34d0f9
Mona Moxie <moxie4lyf@gmail.com>
2025-03-07 09:44:03 +0100

    update footer style

--- home/urls.py
+++ home/urls.py
@@ -14,6 +14,11 @@ urlpatterns = [
         TemplateView.as_view(template_name="home/privacy.html"),
         name="privacy",
     ),
+    path(
+        "terms",
+        TemplateView.as_view(template_name="home/terms.html"),
+        name="terms",
+    ),
     path("meet/<str:public_id>", BookingCalendarView.as_view(), name="meet"),
     path("meet/<str:public_id>/book", BookingView.as_view(), name="book"),
     path(
