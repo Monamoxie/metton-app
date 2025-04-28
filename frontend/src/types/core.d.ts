94064f72d419d12cc0bae6f64fdd9f44d5f05587
Mona Moxie <moxie4lyf@gmail.com>
2025-04-28 09:14:43 +0200

    include SetStateProp in favour of SetIsFinishedProps

--- frontend/src/types/core.d.ts
+++ frontend/src/types/core.d.ts
@@ -6,6 +6,9 @@ export interface ReactSetStateActionProps<T> {
   [key: string | number]: React.Dispatch<React.SetStateAction<T>>;
 }
 
+// todo ::: @depreciated in favour of the generic SetStateProp
 export interface SetIsFinishedProps {
   setIsFinished: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+export type SetStateProp<T> = React.Dispatch<React.SetStateAction<T>>;
