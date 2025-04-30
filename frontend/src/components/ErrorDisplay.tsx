18f6730e34e0a4d01bc4cb1bc528ce0b84272f6d
Mona Moxie <moxie4lyf@gmail.com>
2025-04-30 21:41:58 +0200

    include alert title component to error display component

--- frontend/src/components/ErrorDisplay.tsx
+++ frontend/src/components/ErrorDisplay.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { AlertTitle, Box, Typography } from "@mui/material";
 import Alert from "@mui/material/Alert";
 
 type ErrorProps = {
@@ -13,7 +13,11 @@ export default function ErrorDisplay({ errors, message }: ErrorProps) {
     <Box sx={{ mb: 5 }}>
       {Object.keys(errors).map((key) => (
         <Alert severity="error" key={key}>
-          <strong>{message}</strong>{" "}
+          <AlertTitle>
+            <Typography variant="h6" component="h6">
+              {message}{" "}
+            </Typography>
+          </AlertTitle>
           {Array.isArray(errors[key])
             ? errors[key].map((msg, index) => <div key={index}>{msg}</div>)
             : errors[key]}
