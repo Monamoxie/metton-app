5e11fb8f590b67b8ceaef7fa8d204f4408344b56
Mona Moxie <moxie4lyf@gmail.com>
2025-04-30 21:35:13 +0200

    remove identity fetcher declarations

--- frontend/src/data/outbound/identity-fetcher.ts
+++ frontend/src/data/outbound/identity-fetcher.ts
@@ -4,56 +4,6 @@ import "server-only";
 import { getAuthApiHeader, storeToken } from "@/data/cookie";
 import api from "@/utils/axios-client";
 
-
-// ********** VERIFY EMAIL ********** //
-export async function verifyToken(
-  token: VerifyTokenProps
-): Promise<ApiResponse> {
-  try {
-    const request = await fetch(
-      process.env.API_BASE_URL + "/identity/verification/email",
-      {
-        method: "PATCH",
-        headers: getDefaultApiHeader(),
-        body: JSON.stringify({
-          token,
-        }),
-      }
-    );
-
-    return await request.json();
-  } catch (error: any) {
-    return ApiExceptionHandler(error.message);
-  }
-}
-
-
-// **********  PASSWORD RESET ********** //
-export async function passwordReset({
-  token,
-  password,
-  password_confirmation,
-}: PasswordResetProps): Promise<ApiResponse> {
-  try {
-    const request = await fetch(
-      process.env.API_BASE_URL + "/identity/password-reset",
-      {
-        method: "PATCH",
-        headers: getDefaultApiHeader(),
-        body: JSON.stringify({
-          token,
-          password,
-          password_confirmation,
-        }),
-      }
-    );
-
-    return await request.json();
-  } catch (error: any) {
-    return ApiExceptionHandler(error.message);
-  }
-}
-
 // ********** GET USER PROFILE ********** //
 export async function getUserProfile(): Promise<ApiResponse> {
   try {
