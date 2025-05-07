e2e68733369597890990f14ba8b05ea1f214354c
Mona Moxie <moxie4lyf@gmail.com>
2025-05-07 09:26:53 +0200

    handle user profile update

--- frontend/src/services/user-service.ts
+++ frontend/src/services/user-service.ts
@@ -3,11 +3,35 @@ import { ApiResponse } from "@/types/api";
 import * as Utils from "@/utils/utils";
 
 // -- // --
-export const getUserProfile = async (): Promise<ApiResponse> => {
+export const getProfile = async (): Promise<ApiResponse> => {
   try {
     const response = await axiosClient.get("/identity/user/profile", {
       headers: Utils.getAuthApiHeader(),
     });
+    return response.data;
+  } catch (error: any) {
+    if (error.response?.data) {
+      return error.response.data;
+    }
+    return Utils.ApiExceptionHandler(error.message);
+  }
+};
+
+// -- // --
+export const updateProfile = async (
+  payload: FormData
+): Promise<ApiResponse> => {
+  try {
+    const response = await axiosClient.patch(
+      "/identity/user/profile",
+      payload,
+      {
+        headers: {
+          ...Utils.getAuthApiHeader(),
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
     return response.data;
   } catch (error: any) {
     if (error.response?.data) {
