99cad9ba236fa585950f5c21a4439124843a0723
Mona Moxie <moxie4lyf@gmail.com>
2025-05-19 09:34:40 +0200

    event page

---
+++ frontend/src/app/(secure-area)/events/page.tsx
@@ -0,0 +1,12 @@
+import EventCard from "@/components/event/EventCard";
+import { Card, Container, Stack } from "@mui/material";
+
+export default function EventPage() {
+  return (
+    <Stack>
+      {/* <Card> */}
+      <EventCard />
+      {/* </Card> */}
+    </Stack>
+  );
+}
