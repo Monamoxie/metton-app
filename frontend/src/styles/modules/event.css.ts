e4907203fd773b6f8108bc4e66cca0a12dddd45d
Mona Moxie <moxie4lyf@gmail.com>
2025-05-23 09:33:04 +0200

    event calendar css module

---
+++ frontend/src/styles/modules/event.css.ts
@@ -0,0 +1,15 @@
+import { Theme } from "@mui/system";
+import { SxProps } from "@mui/material";
+
+export const EVENT_CALENDAR = {
+  "& .event-closed-slots": {
+    color: "#fff",
+    background: "#f44336",
+    opacity: 0.8,
+    fontWeight: "bold",
+    fontSize: "0.8em",
+    textAlign: "center",
+    borderRadius: 4,
+    padding: 2,
+  },
+};
