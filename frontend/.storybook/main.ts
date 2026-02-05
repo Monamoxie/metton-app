6b1aca1ea7d01339a9c691c7edeeea90d63c3282
Mona Moxie <moxie4lyf@gmail.com>
2026-02-05 23:20:24 +0000

    handle package upgrades

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,9 +5,7 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/nextjs",
