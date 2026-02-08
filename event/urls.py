d91928ee87e9a3c216349519e34fb031b6fcd279
Mona Moxie <moxie4lyf@gmail.com>
2026-02-08 23:33:06 +0000

    fic breaking change with url path

--- event/urls.py
+++ event/urls.py
@@ -4,5 +4,5 @@ from home.views.booking_view import BookingView
 
 # @depreciated: migrating urls to DRF api
 urlpatterns = [
-    path("<str:public_id>/book", BookingView().as_view(), name="book"),
+    path("<str:public_id>/book", BookingView.as_view(), name="book"),
 ]
