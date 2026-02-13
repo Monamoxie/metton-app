219c14d4b74f309d99985ce68e9b954684a4ae02
Mona Moxie <moxie4lyf@gmail.com>
2026-02-13 21:39:16 +0000

    fix broken issues with stories

--- frontend/src/stories/Page.stories.ts
+++ frontend/src/stories/Page.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent, expect } from '@storybook/test';
+import { within, userEvent, expect } from 'storybook';
 
 import { Page } from './Page';
 
