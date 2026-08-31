b942e88f9870a04842ef9d5a514336f029e41f87
Mona Moxie <moxie4lyf@gmail.com>
2026-08-31 21:10:28 +0100

    feat: set ssl use

--- src/api/core/settings.py
+++ src/api/core/settings.py
@@ -238,7 +238,8 @@ EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 SERVER_EMAIL = os.environ.get("SERVER_EMAIL")
-EMAIL_USE_SSL = True
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "True") == "True"
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "True"
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 DEFAULT_FROM_NAME = os.environ.get("DEFAULT_FROM_NAME")
 
